ea66217b9773e4eefd03aa7faff9f23e88d55040
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2024-10-16 12:44:08 +0300

    refactor(admin): add example usages of new components from enes-sh to page, remove components.json

--- apps/admin/app/(routes)/page.tsx
+++ apps/admin/app/(routes)/page.tsx
@@ -1,4 +1,27 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@enes-sh/ui'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  ScrollArea,
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@enes-sh/ui'
 
 export default async function Page() {
   return (
@@ -15,6 +38,42 @@ export default async function Page() {
           <p>Card Footer</p>
         </CardFooter>
       </Card>
+      <Collapsible>
+        <CollapsibleTrigger>Can I use this in my project?</CollapsibleTrigger>
+        <CollapsibleContent>
+          Yes. Free to use for personal and commercial projects. No attribution required.
+        </CollapsibleContent>
+      </Collapsible>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <ScrollArea className='h-[200px] w-[350px] rounded-md border p-4'>
+        Jokester began sneaking into the castle in the middle of the night and leaving jokes all
+        over the place: under the king's pillow, in his soup, even in the royal toilet. The king was
+        furious, but he couldn't seem to stop Jokester. And then, one day, the people of the kingdom
+        discovered that the jokes left by Jokester were so funny that they couldn't help but laugh.
+        And once they started laughing, they couldn't stop.
+      </ScrollArea>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
   )
 }
