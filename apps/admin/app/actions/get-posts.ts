177d617c91776709cadc23a80ec881616aa86efd
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2024-10-21 08:38:35 +0300

    feat(admin): add get posts server action

---
+++ apps/admin/app/actions/get-posts.ts
@@ -0,0 +1,31 @@
+import { PaginationParams } from '@/app/types'
+import { prisma } from '@enes-sh/db'
+
+const getPosts = async ({ page = 1, pageSize = 10 }: PaginationParams) => {
+  const skip = (page - 1) * pageSize
+
+  const posts = await prisma.content.findMany({
+    where: {
+      type: 'POST'
+    },
+    skip,
+    take: pageSize,
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  const totalPosts = await prisma.content.count({ where: { type: 'POST' } })
+
+  return {
+    posts,
+    pagination: {
+      totalPosts,
+      currentPage: page,
+      pageSize,
+      totalPages: Math.ceil(totalPosts / pageSize)
+    }
+  }
+}
+
+export { getPosts }
