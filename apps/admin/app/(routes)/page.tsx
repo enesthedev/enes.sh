be5fb07578704f9b27cfff298b2a51436def3e0b
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2024-10-25 23:46:15 +0300

    feat(admin): add posts count card at overview screen

--- apps/admin/app/(routes)/page.tsx
+++ apps/admin/app/(routes)/page.tsx
@@ -1,10 +1,17 @@
+import { CountedCardSkeleton } from '@/app/components'
 import { ContentLayout, SidebarLayout } from '@/app/components/layouts'
+import { Suspense } from 'react'
+import { PostsCountCard } from './posts-count-card'
 
 export default async function Page() {
   return (
     <SidebarLayout>
       <ContentLayout title='test'>
-        <div className='h-[2000px]'>test</div>
+        <div className='mt-0 grid sm:grid-cols-2 lg:grid-cols-3'>
+          <Suspense fallback={<CountedCardSkeleton />}>
+            <PostsCountCard />
+          </Suspense>
+        </div>
       </ContentLayout>
     </SidebarLayout>
   )
