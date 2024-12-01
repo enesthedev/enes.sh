6d01338d307e22d1aa1aef732fb7544ab0f4aaa7
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2024-12-01 23:57:19 +0300

    refactor: remove graphql feature with publication queries and hooks

--- app/features/publication/components/publication-landing-details.tsx
+++ app/features/publication/components/publication-landing-details.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import { env } from '@/env';
-import { useGetPublicationQuery } from '../../graphql/__generated__/graphql';
-
 export const PublicationLandingDetails = () => {
-  const { data, loading } = useGetPublicationQuery({
-    variables: { host: env.NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST },
-  });
   return (
     <div>
       <h1>Publication Landing Details</h1>
-      {data?.publication?.title}
-      {loading.toString()}
+      {/*data?.publication?.title
+      {loading.toString() */}
     </div>
   );
 };
